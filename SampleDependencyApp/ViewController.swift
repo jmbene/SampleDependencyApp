7c503122867806da6dbdc7cdd4fd216c45c3aab3
Jose Miguel Benedicto Ruiz <jmbene@gmail.com>
2017-12-13 04:08:09 +0100

    Update Sample ViewController
    Update Sample ViewController

--- SampleDependencyApp/ViewController.swift
+++ SampleDependencyApp/ViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 import SampleDependencySDK
 
 class ViewController: UIViewController {
-
+    
+    var caller = SampleCall()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        SampleCall().call(completion: { (result) in
+        caller.call(completion: { (result) in
             print(result)
         })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
